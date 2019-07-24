76fe6e5b9ae3ede6682cfd1cf760cb19da80b7a4
KennethDyerHill <52476371+KennethDyerHill@users.noreply.github.com>
2019-07-24 10:57:43 -0400

    Update 24-7-19 1057

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,8 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+
+
 /* 
 
   //Given this problem: 
@@ -53,12 +55,18 @@ sumNums(1, 2, function(addNums) {
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+multiplyNums(1, 2, function(multNums) {
+  console.log(multNums);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+return cb(item, list);
 }
+contains()
 
 /* STRETCH PROBLEM */
 
